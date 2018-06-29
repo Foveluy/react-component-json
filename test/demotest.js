a5dcc92c924ad6fdc87148c061afeaccf197f1e3
zhengfang <215566435@qq.com>
2018-06-29 15:39:13 +0800

    chore: demo abstractor

---
+++ test/demotest.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Tab from ".";
+/**
+ * demo: 一般场景
+ */
+
+const Normal = () => <Tab tabs={Tabs} />;
+
+/**
+ * demo: 一般场景2
+ */
+const Tabs = [{ title: "First" }, { title: "Second" }, { title: "Third" }];
+
+/**
+ * demo: 一般场景3
+ */
+const Tabs3 = [{ title: "First" }, { title: "Second" }, { title: "Third" }];
+const Tabs4 = [{ title: "First" }, { title: "Second" }, { title: "Third" }];
+
+export default TabsDemo;
--- test/test.js
+++ test/test.js
@@ -1,10 +1,14 @@
-const { json, md } = require("../src/parser");
+const { json, md, demo } = require("../src/parser");
 const fs = require("fs");
 
 const source = fs.readFileSync("./time.js", "utf8");
+const demosrc = fs.readFileSync("./demotest.js", "utf8");
+
 
 const obj = json(source);
 
 fs.writeFileSync("./test.json", JSON.stringify(obj, null, 2));
 
 fs.writeFileSync("./test.md", md(obj));
+
+const d = demo(demosrc);
